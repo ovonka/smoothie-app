bd18b81813c7418235aa773092f1a1340b0b9c32
Nkanyiso Ntshangase <nkanyisontshangase349@gmail.com>
2022-10-10 13:58:06 +0200

    added product details page using router-dom

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -16,8 +16,30 @@ const ProductDetails = () => {
       <div className="description">
         <h3>Strawberry Cinnamon Shake</h3>
         <span>rating</span>
-        <span>R40</span>
-        <span>Quantity</span>
+        <span>
+          {" "}
+          <strong> R40</strong>
+        </span>
+        <span>Quantity:</span>
+        <div className="add-product-box">
+          <span className="count-up">+</span>
+          <div className="number-item">
+            <span>0</span>
+          </div>
+          <span className="count-down">-</span>
+        </div>
+        <button>Add To Cart</button>
+        <span className="pro-detail">
+          As a healthy food provider, we are happy to have helped thousands of
+          people get their weight and health back on track by eating well
+          portioned and healthy meals. It is a lifestyle we now live and preach
+          to our clients, family, and friends every day.
+        </span>
+        <div className="social-media">
+          <span>Twitter</span>
+          <span>Facebook</span>
+          <span>Instagram</span>
+        </div>
       </div>
     </div>
   );
